2ef99e80fdb70be77470dcac1acd8358743c532c
Arpad Magosanyi <m4gw4s@gmail.com>
2022-12-14 13:39:21 +0100

    initial checkin

---
+++ src/UI/MainUI.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export default class MainUI extends React.Component {
+	render () {
+	  return (
+		<div>
+		<h1>Hello, I am testing react</h1>
+		</div>
+	  );
+        }
+}
+
+
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import MainUI from "./UI/MainUI"
+ReactDOM.render(
+<MainUI/>,
+  document.getElementById("root")
+);
