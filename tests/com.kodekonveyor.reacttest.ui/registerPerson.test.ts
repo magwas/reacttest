ab38c00515936faec387f45375646be156e27be6
Builder Daemon <m4gw4s+buildmachine@gmail.com>
2022-12-20 18:49:59 +0000

    jest working

---
+++ tests/com.kodekonveyor.reacttest.ui/registerPerson.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from '@jest/globals';
+import { AnyAction } from '@reduxjs/toolkit';
+import { mock, mockFn } from 'jest-mock-extended';
+
+import * as React from "react";
+import { registerPerson } from "../../src/com.kodekonveyor.reacttest.ui/actions/registerPerson";
+import { actions } from '../../src/com.kodekonveyor.reacttest.ui/state/appState';
+import { Person } from '../../src/com.kodekonveyor.reacttest.ui/state/types/Person';
+import { NameInputFormUIStateInitializer } from '../../src/com.kodekonveyor.reacttest.ui/UI/NameInputFormUIStateInitializer';
+
+describe('registerPerson', function () {
+
+  const theEvent = mock<React.FormEvent<Element>>();
+  const formState = mock(Person);
+  const setState = mockFn<React.Dispatch<React.SetStateAction<Person>>>();
+  const dispatch = mockFn<React.Dispatch<AnyAction>>();
+
+  registerPerson(theEvent, formState, setState, dispatch);
+
+  it('prevents automatic redraw of the form', function () {
+    expect(theEvent.preventDefault).toHaveBeenCalled();
+  });
+
+  it('adds the person from the form to the person list', function () {
+    expect(dispatch).toHaveBeenCalledWith(actions.addPerson(formState))
+  });
+
+  it('reinitializes the form data', function () {
+    expect(setState).toHaveBeenCalledWith(NameInputFormUIStateInitializer);
+  });
+
+
+});
